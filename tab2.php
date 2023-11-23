0f1dbc6807f1610333a3b67d9f401de704e8a684
ceslo <slovesce@gmail.com>
2023-11-23 14:16:18 +0100

    avec le nombre de pays

--- tab2.php
+++ tab2.php
@@ -124,7 +124,10 @@
 echo"<table class=\"table table-bordered table-striped text-center\"><tr><td>Capitales</td><td>Pays</td></tr>";
 asort($capitales);
 foreach ($capitales as $villes =>$pays)
-{echo"<tr><td>$pays</td><td>$villes</td></tr>";};
+{echo"<tr><td>$pays</td><td>$villes</td></tr>";
+};
+$n=count($capitales);
+echo "<tr><tr><td>Nombre de pays</td><td>$n</td></tr>";
 echo "</table>";
 echo "<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL\" crossorigin=\"anonymous\"></script>"
 ?> 

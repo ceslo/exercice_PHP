093daabe93a4f1534e0c2bd849311392c1237f8d
ceslo <slovesce@gmail.com>
2023-11-28 14:43:44 +0100

    corrigé terminé

--- tab3.php
+++ tab3.php
@@ -23,12 +23,14 @@ echo"<table class=\"table table-bordered table-striped text-center\"><tr><td>Reg
 foreach ($departements as $region =>$departementsList)
 { 
     sort($departementsList);
-    echo"<tr><td>$region<td>";
+    echo"<tr><td>$region</td><td>";
     for($index=0; $index<=count($departementsList)-1; $index++)
     {
         echo "$departementsList[$index]<br>";
     };
+    echo "</td></tr>";
 };
+echo "</table>";
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js\" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL\" crossorigin="anonymous"></script>
 </body>

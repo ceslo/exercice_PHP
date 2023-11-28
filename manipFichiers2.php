18c338b02afc61a7b94a7be367092d8729bb1595
ceslo <slovesce@gmail.com>
2023-11-28 10:51:58 +0100

    TABLEAU terminé

--- manipFichiers2.php
+++ manipFichiers2.php
@@ -3,22 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
     <?php
+    echo "<table class=\"table table-bordered text-center table-striped\"><tr><th>SURNAME</th><th>FIRSTNAME</th><th>EMAIL</th><th>PHONE</th><th>CITY</th><th>STATE</th></tr>";
 $newUsers=file("https://ncode.amorce.org/customers.csv");
 // var_dump($newUsers);
+
 foreach($newUsers as $index => $user){
-    // var_dump($user);
+    // var_dump($user);   
     $userInfo= explode(",",$user);
-    $userInfo[0]=$surname;
-    $userInfo[1]=$firstName; 
-    $userInfo[2]=$email;
-    $userInfo[3]=$city;
-    $userInfo[4]=$state;
-    echo"<tr><td>$surname</td> <td>$firstName</td> <td>$email</td> <td>$city</td> <td>$state</td></tr>";
+    // var_dump($userInfo);    
+    $surname=$userInfo[0];
+    $firstName=$userInfo[1]; 
+    $email=$userInfo[2];
+    $phone=$userInfo[3];
+    $city=$userInfo[4];
+    $state=$userInfo[5];
+    echo"<tr><td>$surname</td> <td>$firstName</td> <td>$email</td>  <td>$phone</td><td>$city</td> <td>$state</td></tr>";
 };
+echo "</table>";
     ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
